9c174a0167613063be37a0d2b374b60ecdbb2a10
Alla <alla.reshetina@gmail.com>
2020-10-06 20:31:23 +0300

    Add tests

---
+++ Examples/Testing/UnitTests/DayOfWeekServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using UnitTestsTarget;
+
+namespace UnitTests
+{
+    public class DayOfWeekServiceTests
+    {
+        [Test]
+        public void IsWeekend_Sunday_True()
+        {
+            var service = new DayOfWeekService();
+            const bool expected = true;
+            
+            var sunday = new DateTime(2020, 10, 03);
+            var actual = service.IsWeekend(sunday);
+
+            actual.Should().Be(expected);
+        }
+        
+        [Test]
+        public void IsWeekend_Monday_False()
+        {
+            const bool expected = false;
+            var service = new DayOfWeekService();
+            
+            var monday = new DateTime(2020, 10, 02);
+            var actual = service.IsWeekend(monday);
+
+            actual.Should().Be(expected);
+        }
+
+    }
+}
---
+++ Examples/Testing/UnitTests/DayShiftServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using UnitTestsTarget;
+
+namespace UnitTests
+{
+    public class DayShiftServiceTests
+    {
+        private readonly DateTime friday = new DateTime(2020, 10, 02);
+        private readonly DateTime monday = new DateTime(2020, 10, 05);
+        private readonly DateTime tuesday = new DateTime(2020, 10, 06);
+
+        
+        IDayShiftService service;// = new DayShiftService(dayOfWeekService);
+
+        [SetUp]
+        public void SetUp()
+        {
+            IDayOfWeekService dayOfWeekService = new DayOfWeekService();
+            service = new DayShiftService(dayOfWeekService);    
+        }
+        
+        [Test]
+        public void GetShiftBusinessDay_RealLogic_MondayToTuesday()
+        {
+            var actual = service.GetShiftBusinessDay(monday, 1);
+
+            actual.Should().Be(tuesday);
+        }
+        
+        [Test]
+        public void GetShiftBusinessDay_RealLogic_TuesdayToMonday()
+        {
+            var actual = service.GetShiftBusinessDay(tuesday, -1);
+
+            actual.Should().Be(monday);
+        }
+        
+        [Test]
+        public void GetShiftBusinessDay_RealLogic_FridayToMonday()
+        {
+            var actual = service.GetShiftBusinessDay(friday, 1);
+
+            actual.Should().Be(monday);
+        }
+        
+        [Test]
+        public void GetShiftBusinessDay_RealLogic_MondayToFriday()
+        {
+            var actual = service.GetShiftBusinessDay(monday, -1);
+
+            actual.Should().Be(friday);
+        }
+    }
+}
